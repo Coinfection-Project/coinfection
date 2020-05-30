adf4bfdc07019ea7a5390e4488a01b1dc25757b2
Lydia Fox <66090474+lydia-fox@users.noreply.github.com>
2020-05-30 16:34:00 +0100

    add diff test

--- src/difficulty.py
+++ src/difficulty.py
@@ -8,6 +8,9 @@ Homestead eth pow difficulty algo
 Thanks to eth and https://github.com/giact/ for this diff algo
 https://github.com/giact/ethereum-blocktime-simulator/blob/master/ethereum-blocktime-simulator.py
 '''
+from block import *
+import time
+
 def compute_difficulty(block, blockparent):
     d0 = 131072  # difficulty floor as defined by the protocol
     x = blockparent.difficulty / 2048
@@ -19,3 +22,21 @@ def compute_difficulty(block, blockparent):
         # current protocol
         sigma = max(1 - (block.timestamp - blockparent.timestamp) / 10, -99)
     return max(d0, blockparent.difficulty + sigma * x)
+
+def difficulty_test():
+    BLOCKCHAIN = []
+    first = True
+    while True:
+        if (first == False):
+            print("creating new block")
+            pb = BLOCKCHAIN[-1]
+            diff = 1
+            if (len(BLOCKCHAIN) > 1):
+                diff = compute_difficulty(pb, BLOCKCHAIN[-2])
+            new_block = Block(height=pb.height+1, hash='', diff_bits=diff, timestamp=time.time()*1000, transactions=[], nonce=0, version=100, prev_hash=pb.hash)
+            print("Created block, mining")
+            block.mine()
+            print("Mined block. hash={} time={} difficulty={}".format(new_block.hash, (time.now()*1000)-new_block.time, new_block.diff_bits))
+                
+                
+                
