92b7407b237794dcacfe5bc4b0b460f909ad7815
Lydia Fox <66090474+lydia-fox@users.noreply.github.com>
2020-05-30 17:12:03 +0100

    Create wallet_utils.py

---
+++ src/wallet_utils.py
@@ -0,0 +1,21 @@
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
+
+class Wallet:
+  balance = 0
+  outputs = []
+  pubkey = ''
+  privkey = ''
+  address = ''
+  def __init(balance=0, outputs=[], pubkey='', privkey='', address=''):
+    self.balance = balance
+    self.outputs = outputs
+    self.pubkey = pubkey
+    self.privkey = privkey
+    self.address = address
+  def gen_keypair(self):
+    self.privkey = Ed25519PrivateKey.generate()
+    self.pubkey = private_key.public_key()
+    self.address = self.pubkey.public_bytes(
+      encoding=serialization.Encoding.Raw,
+      format=serialization.PublicFormat.Raw
+    ).hex()
