512ce2399673d953e34ca29c31db075f4fec3584
Lydia Fox <66090474+lydia-fox@users.noreply.github.com>
2020-05-31 14:34:56 +0100

    Update and rename wallet_utils.py to utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 from cryptography.hazmat.primitives import serialization
+import time as time_ #make sure we don't override time
 
 class Wallet:
   balance = 0
@@ -20,3 +21,7 @@ class Wallet:
       encoding=serialization.Encoding.Raw,
       format=serialization.PublicFormat.Raw
     ).hex()
+
+# thanks to https://stackoverflow.com/a/6000198
+def millis():
+    return int(round(time_.time() * 1000))
